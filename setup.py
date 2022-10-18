ef438d4fc214ec9df14ebfb2bebf9cbe3cb0a815
DamienGulliver <93319660+DamienGulliver@users.noreply.github.com>
2022-10-19 10:55:43 +1100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Curated drug binding kinetics datasets and algorithms to work with them.',
+    author='damiengulliver',
+    license='',
+)
